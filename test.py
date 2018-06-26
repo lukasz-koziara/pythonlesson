f936ad6b8d84c113bb17ea2122a41edeba9dd269
Koza88 <lukasz.koziara88@gmail.com>
2018-06-26 20:23:29 +0200

    VAT practice

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
-wiek = 30
-print(wiek)
-wiek = 29
-print(wiek)
+VAT = 23
+obliczonyVAT = (1 + VAT / 100)
+
+cenaNettoJava = 10
+cenaNettoAjax = 20
+cenaNettoJs = 30
+cenaNettoHTML = 10
+
+cenaBruttoJava = cenaNettoJava * obliczonyVAT
+cenaBruttoAjax = cenaNettoAjax * obliczonyVAT
+cenaBruttoJs = cenaNettoJs * obliczonyVAT
+cenaBruttoHTML = cenaNettoHTML * obliczonyVAT
+
+print(cenaBruttoAjax)
+print(cenaBruttoJava)
+print(cenaBruttoHTML)
+print(cenaBruttoJs)
