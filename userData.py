6ad499f5d8c922d36eb7412c680df15bab185faa
Koza88 <lukasz.koziara88@gmail.com>
2018-06-27 11:18:14 +0200

    inputs with user data

---
+++ userData.py
@@ -0,0 +1,14 @@
+imie = str(input('Jak masz na imie?'))
+wiek = int(input('Ile masz lat ?'))
+
+print('Czesc', imie, 'masz', wiek, 'lat')
+
+
+
+
+print('Dodawanie')
+
+a = int(input('Podaj pierwszą liczbe: '))
+b = int(input('Podaj druga liczbe: '))
+
+print('Wynik dodawania to:', a, '+', b, 'to:' , a + b)
