a149ae309dc85718a32653b5b70a8dcc3e924520
Koza88 <lukasz.koziara88@gmail.com>
2018-06-28 11:35:30 +0200

    condition loop

--- loops.py
+++ loops.py
@@ -1,9 +1,10 @@
 wynik = 0
-
-i = 0
-while i < 4:
-    x = int(input('Podaj liczbe: '))
-    wynik += x
-    i +=1
-
-print('Wynik dodawania to: ', wynik)
+for i in range(100):
+    if (i % 6 == 0) and (i % 4 != 0):
+        print('Liczba', i, ' jest podzielna przez 6, ale nie jest podzielna przez 4')
+    elif (i % 6 != 0) and (i % 4 == 0): 
+        print('Liczba', i, ' nie jest podzielna przez 6, ale jest podzielna przez 4')
+    elif (i % 6 == 0) and (i % 4 == 0):
+        print('Liczba', i, ' jest podzielna przez 6 i przez 4')
+    else:
+        print('Liczba', i, 'nie dzieli sie przez 4 ani przez 6')
